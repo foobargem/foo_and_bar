61356b4acbf73e5507487898d9503a6465fcb208
Eunchul Jung <agfe09@gmail.com>
2011-03-08 11:15:46 +0900

    add: flickr auth

---
+++ sms_web/app/controllers/flickr_auth_controller.rb
@@ -0,0 +1,18 @@
+class FlickrAuthController < ApplicationController
+
+  def new
+    FlickRaw.api_key = FLICKR_APP_KEY
+    FlickRaw.shared_secret = FLICKR_APP_SECRET
+    auth_url = FlickRaw.auth_url :perms => "write"
+    redirect_to auth_url
+  end
+
+  def callback
+    frob = params[:frob]
+    auth = flickr.auth.getToken :frob => frob
+    session[:flickr_auth_token] = auth
+
+    redirect_to root_path
+  end
+
+end
---
+++ sms_web/test/unit/helpers/flickr_auth_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class FlickrAuthHelperTest < ActionView::TestCase
+end

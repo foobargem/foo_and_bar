2261b7ddbcafb82027a9abebe95a9d4cb1b4cb9e
Eunchul Jung <agfe09@gmail.com>
2011-03-09 14:03:16 +0900

    update: applicant validation

--- sms_web/app/models/applicant.rb
+++ sms_web/app/models/applicant.rb
@@ -2,6 +2,16 @@ class Applicant < ActiveRecord::Base
 
   belongs_to :event
 
-  validates_presence_of :name, :address, :phone_number
+  validates_presence_of :name, :address, :phone_number,
+                        :message => I18n.t("activerecord.errors.messages.blank")
+  validates_uniqueness_of :phone_number,
+                          :message => I18n.t("activerecord.errors.messages.already_taken_phone_number")
+
+  protected
+
+    def sanitize_phone_number
+      self.phone_number = self.phone_number.gsub(/\D/, '')
+    end
+    before_save :sanitize_phone_number
 
 end

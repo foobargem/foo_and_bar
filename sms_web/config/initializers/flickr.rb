cf6b73b1184e7c587abdbe75a5f983f9faa926be
Eunchul Jung <agfe09@gmail.com>
2011-03-11 21:32:25 +0900

    update: page styles & flickr api key

--- sms_web/config/initializers/flickr.rb
+++ sms_web/config/initializers/flickr.rb
@@ -1,4 +1,9 @@
 
-FLICKR_APP_KEY = "70f009b8e642750f1be183deafac4eb1"
-FLICKR_APP_SECRET = "9db2c4c4f87940a0"
+if Rails.env == "production"
+  FLICKR_APP_KEY = "70f009b8e642750f1be183deafac4eb1"
+  FLICKR_APP_SECRET = "9db2c4c4f87940a0"
+else
+  FLICKR_APP_KEY = "ca4e575b739f6930c72f43df498cc755"
+  FLICKR_APP_SECRET = "eddf2c3bf5e389fe"
+end
 

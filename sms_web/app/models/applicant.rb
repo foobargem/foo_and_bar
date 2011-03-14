f47349824234e35672009e4e3474475d61cdd601
Eunchul Jung <agfe09@gmail.com>
2011-03-14 17:33:34 +0900

    update: default error message

--- sms_web/app/models/applicant.rb
+++ sms_web/app/models/applicant.rb
@@ -16,10 +16,8 @@ class Applicant < ActiveRecord::Base
 
   belongs_to :event, :counter_cache => true
 
-  validates_presence_of :name, :address, :phone_number,
-                        :message => I18n.t("activerecord.errors.messages.blank")
-  validates_uniqueness_of :phone_number,
-                          :message => I18n.t("activerecord.errors.messages.already_taken_phone_number")
+  validates_presence_of :name, :address, :phone_number
+  validates_uniqueness_of :phone_number, :scope => :event_id
 
 
   default_scope :order => "id DESC"

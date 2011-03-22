378b007f5a210d6b0684e9a455f05b333ba93e5c
Eunchul Jung <agfe09@gmail.com>
2011-03-22 19:26:00 +0900

    update: admin/racing_models - published

--- sms_web/app/helpers/admin/racing_models_helper.rb
+++ sms_web/app/helpers/admin/racing_models_helper.rb
@@ -1,2 +1,11 @@
 module Admin::RacingModelsHelper
+
+  def display_racing_model_flickr_image(r)
+    unless r.photo_thumb_url.nil?
+      image_tag r.photo_thumb_url, :size => "80x80"
+    else
+      "-"
+    end
+  end
+
 end

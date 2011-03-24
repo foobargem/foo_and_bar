de5c5f72035b6c045b547c4e8baf385601a9a030
Eunchul Jung <agfe09@gmail.com>
2011-03-24 15:05:09 +0900

    update: end of event

--- sms_web/app/controllers/applicants_controller.rb
+++ sms_web/app/controllers/applicants_controller.rb
@@ -6,15 +6,18 @@ class ApplicantsController < ApplicationController
   def create
     #redirect_to root_path and return unless request.xhr?
 
-    @applicant = Applicant.new(params[:applicant])
-    @applicant.user_agent = request.env["HTTP_USER_AGENT"]
-    result = if @applicant.save
-              { :status => "Success",
-                :message => "응모완료 되었습니다." }
-             else
-              { :status => "Failed",
-                :message => @applicant.errors.full_messages.first }
-             end
+    #@applicant = Applicant.new(params[:applicant])
+    #@applicant.user_agent = request.env["HTTP_USER_AGENT"]
+    #result = if @applicant.save
+    #          { :status => "Success",
+    #            :message => "응모완료 되었습니다." }
+    #         else
+    #          { :status => "Failed",
+    #            :message => @applicant.errors.full_messages.first }
+    #         end
+
+    result = { :status => "이벤트 종료",
+               :message => "이벤트가 23일자로 종료되었습니다." }
 
     render :json => result.to_json
   end

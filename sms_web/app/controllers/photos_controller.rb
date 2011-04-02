9f9bb795085c0e9ebc3377d66c30cc771636f754
Eunchul Jung <agfe09@gmail.com>
2011-04-02 18:40:39 +0900

    update: models_by_company

--- sms_web/app/controllers/photos_controller.rb
+++ sms_web/app/controllers/photos_controller.rb
@@ -35,18 +35,23 @@ class PhotosController < ApplicationController
   def models_by_company
     company = Company.find(params[:company_id])
 
-    car_ids = company.cars.select("id").map(&:id)
-    scoped = Photo.with_car_and_racing_model.where("car_id in (?)", car_ids)
+    @racing_models = company.racing_models.
+                      select("id, photo_thumb_url")
 
-    @photos = scoped.
-                select("id, racing_model_id, thumb_url")
-                #paginate(:page => params[:page], :per_page => 20)
-
-    #last_photo = scoped.last
-    #has_next = (last_photo.nil? || @photos.last.id == last_photo.id) ? "n" : "y"
+    photos = []
+    @racing_models.each do |racing_model|
+      photos << { "photo" => {
+          "racing_model_id" => racing_model.id,
+          "thumb_url" => racing_model.photo_thumb_url
+        }
+      }
+    end
 
     respond_to do |format|
-      format.json { render :json => @photos.to_json }
+      format.json { render :json => {
+          :photos => photos
+        }.to_json
+      }
     end
   end
 
